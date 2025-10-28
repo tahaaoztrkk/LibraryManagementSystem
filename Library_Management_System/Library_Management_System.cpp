08539eb1da87f7e2939422f1591fdbf116377a5a
tahaaoztrkk <tahaa.oztrkk@gmail.com>
2025-10-28 23:24:48 +0300

    completed the task

--- Library_Management_System/Library_Management_System.cpp
+++ Library_Management_System/Library_Management_System.cpp
@@ -81,11 +81,91 @@ public:
 		file.close();
 		cout << "Total book is: " << bookcount << endl;
 	}
-	
+	void saveBooksToFile() {
+		ofstream file(bookFileName);
 
+		while (file.is_open())
+		{
+			for (int i = 0; i < bookcount; i++) {
+				file << bookCatalog[i].getSaveFormat()<<endl;
+			}
+		}
+	}
+	void listAllBooks() {
+		for (int i = 0; i < bookcount; i++) {
+			bookCatalog[i].display();
+		}
+	}
+	void borrowBook(int bookID) {
+		for (int i = 0; i < bookcount; i++) {
+			if (bookCatalog[i].getID() == bookID) {
+				if (bookCatalog[i].isAvailable()) {
+					bookCatalog[i].borrowBook();
+					cout << "Book borrowed succesfully."<<endl;
+				}
+				else {
+					cout << "Book is already borrowed.";
+				}
+			}
+			else {
+				cout << "Book not found.";
+			}
+		}
+	}
+	void returnBook(int bookID) {
+		for (int i = 0; i < bookcount; i++) {
+			if (bookCatalog[i].getID() == bookID) {
+				if (!bookCatalog[i].isAvailable()) {
+					bookCatalog[i].borrowBook();
+					cout << "Book returned succesfully.";
+				}
+				else {
+					cout << "Book is already borrowed.";
+				}
+			}
+			else {
+				cout << "Book not found.";
+			}
+		}
+	}
+	
 };
 
 int main() {
+	Library myLibrary("books.txt");
+	int selection,id;
+	do
+	{
+		cout << "--------Library Menu--------"<<endl;
+		cout << "1.List All Books"<<endl;
+		cout << "2. Borrow a Book" << endl;
+		cout << "3.Return a Book" << endl;
+		cout << "Save and Exit" << endl;
+		cin >> selection;
 
+		switch (selection)
+		{
+		case 1:
+			myLibrary.listAllBooks();
+			break;
+
+		case 2:
+			cout << "Please enter book ID: ";
+			cin >> id;
+			myLibrary.borrowBook(id);
+			break;
+
+		case 3:
+			cout << "Please enter book ID: ";
+			cin >> id;
+			myLibrary.returnBook(id);
+			break;
+
+		case 4:
+			myLibrary.saveBooksToFile();
+			cout << "Saving changes... Goodbye!";
+			break;
+		}
+	} while (selection!=4);
 
 }
